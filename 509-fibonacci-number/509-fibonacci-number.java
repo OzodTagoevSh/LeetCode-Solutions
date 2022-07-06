b8bd363748c0283a8036741a119b3bbcbf19b8a7
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2022-07-05 23:56:11 -0500

    Time: 0 ms (100.00%), Space: 41.2 MB (28.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n<=2) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n<=1) return n;
+        if(n==2) return 1;
+        int f1 = 1;
+        int f2 = 1;
+        int fn = 2;
+        for(int i=3; i<=n; i++) {
+            fn = f1 + f2;
+            f1 = f2; 
+            f2 = fn;
+        }
+        return fn;
     }
 }

7e1d185a61dec368c971ef551d72807f4033c5cf
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2022-08-17 16:03:05 -0500

    Time: 0 ms (100.00%), Space: 44.8 MB (37.98%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.java
+++ 328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -13,16 +13,12 @@ class Solution {
         if(head==null || head.next==null) return head;
         ListNode first = head, second = head.next;
         ListNode newHead = second;
-        head = first;
-        while(first.next!=null && second.next!=null) {
+        while(second!=null && second.next!=null) {
             first.next = second.next;
             first = first.next;
             second.next = first.next;
             second = second.next;
         }
-        if(first!=null) first.next=null;
-        first = head;
-        while(first.next!=null) first=first.next;
         first.next=newHead;
         return head;
     }
